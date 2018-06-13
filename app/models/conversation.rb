764ae67f5fc88ba2481282b5d5e30e28ad2d3a06
swojeet <swojeetkayastha@gmail.com>
2018-06-13 15:06:41 +0545

    Add scopes and class methods to Conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,6 +5,19 @@ class Conversation < ApplicationRecord
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
+  scope :between, -> (sender_id, recipient_id) do
+    where(sender_id: sender_id, recipient_id: recipient_id).or( where(sender_id: recipient_id, recipient_id: sender_id))
+  end
+
+  def self.get(sender_id, recipient_id)
+    # return the conversation between the sender and the recipient
+    conversation = between(sender_id, recipient_id).first
+    return conversation if conversation.present?
+
+    #create a new conversation if a conversation between the sender and the recipient does not exist
+    create(sender_id: sender_id, recipient_id: recipient_id)
+  end
+
   def opposed_user(user)
     user == recipient ? sender : recipient
   end
