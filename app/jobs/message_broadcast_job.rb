2acc330d49aeb0fc4c71d6eeb21cf1986d5bd22b
swojeet <swojeetkayastha@gmail.com>
2018-06-15 15:21:38 +0545

    Add code, to pop up new message and notify user on arribal of new message

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -18,13 +18,20 @@ class MessageBroadcastJob < ApplicationJob
   end
 
   def broadcast_to_recipient(user, message)
-    ActionCable.server.broadcast( "conversations-#{user.id}", message: render_message(message, user), conversation_id: message.conversation_id
+    ActionCable.server.broadcast( "conversations-#{user.id}", window: render_window(message.conversation, user), message: render_message(message, user), conversation_id: message.conversation_id
     )
   end
 
   def render_message(message, user)
     ApplicationController.render( partial: 'messages/message',
     locals: { message: message, user: user }
+    )
+  end
+
+  def render_window(conversation, user)
+    ApplicationController.render(
+      partial: 'conversations/conversation',
+      locals: { conversation: conversation, user: user }
     )
   end
 end
