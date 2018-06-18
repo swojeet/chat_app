b303ff902c130e6b665f6765928197a2c9f98762
swojeet <swojeetkayastha@gmail.com>
2018-06-18 11:41:32 +0545

    Add middle and configure middleware in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.user ChatActionCable
+  config.web_socket_server_url = "wss://rails-actioncable-test.herokuapp.com/"
 end
